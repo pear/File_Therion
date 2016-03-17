a587bac86051b52638fdea916a22943d8f5e353f
hbeni <beni@php.net>
2016-03-17 22:55:26 +0100

    Shot enhanced.

--- File/Therion/Shot.php
+++ File/Therion/Shot.php
@@ -35,13 +35,13 @@ class File_Therion_Shot
     protected $_data = array(
         'from'      => "", // Station
         'to'        => "", // Station
-        'length'    => "",
-        'bearing'   => "",
-        'gradient'  => "",
-        'left'      => "",
-        'right'     => "",
-        'up'        => "",
-        'down'      => "",
+        'length'    => 0.0,
+        'bearing'   => 0.0,
+        'gradient'  => 0.0,
+        'left'      => 0.0,
+        'right'     => 0.0,
+        'up'        => 0.0,
+        'down'      => 0.0,
         // more according to thbook
     );
     
@@ -115,7 +115,7 @@ class File_Therion_Shot
      * @param string  $flag  name of the flag.
      * @throws InvalidArgumentException
      */
-    public function getFlag(string $flag)
+    public function getFlag($flag)
     {
         if ($flag == "approx") {
             // expand alias
@@ -129,6 +129,313 @@ class File_Therion_Shot
         }
     }
     
+    
+    /**
+     * GET DATA
+     */
+     
+     
+    /**
+     * Get from (source) station.
+     * 
+     * @return string
+     */
+    public function getFrom()
+    {
+        return $this->_data['from'];
+    }
+    
+    /**
+     * Get to (targeted) station.
+     * 
+     * @return string
+     */
+    public function getTo()
+    {
+        return $this->_data['to'];
+    }
+    
+    /**
+     * Get shot length.
+     * 
+     * @return float
+     */
+    public function getLength()
+    {
+        return $this->_data['length'];
+    }
+    
+    /**
+     * Get shot compass bearing.
+     * 
+     * @return int between 0 and 360 (when unit was grad)
+     */
+    public function getBearing()
+    {
+        return $this->_data['bearing'];
+    }
+    
+    /**
+     * Get shot gradient.
+     * 
+     * @return float between -90 to 90 (when unit was grad)
+     */
+    public function getGradient()
+    {
+        return $this->_data['gradient'];
+    }
+    
+    /**
+     * Get shot left dimensions.
+     * 
+     * @return float
+     */
+    public function getLeft()
+    {
+        return $this->_data['left'];
+    }
+    
+    /**
+     * Get shot right dimensions.
+     * 
+     * @return float
+     */
+    public function getRight()
+    {
+        return $this->_data['right'];
+    }
+    
+    /**
+     * Get shot up (height to ceiling) dimensions.
+     * 
+     * @return float
+     */
+    public function getUp()
+    {
+        return $this->_data['up'];
+    }
+    
+    /**
+     * Get shot down (hieght to ground) dimensions.
+     * 
+     * @return float
+     */
+    public function getDown()
+    {
+        return $this->_data['down'];
+    }
+
+    
+    
+    
+    /**
+     * SET DATA
+     */
+     
+     /**
+     * Set from (source) station.
+     * 
+     * @param string $station
+     */
+    public function setFrom($station)
+    {
+        if (!is_string($station)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        $this->_data['from'] = $station;
+    }
+    
+    /**
+     * Set to (targeted) station.
+     * 
+     * @param string $station
+     */
+    public function setTo($station)
+    {
+        if (!is_string($station)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        $this->_data['to'] = $station;
+    }
+    
+    /**
+     * Set shot length.
+     * 
+     * @param float $length
+     */
+    public function setLength($length)
+    {
+        // convert to explicit type
+        $length = (is_string($length)||is_int($length))? floatval($length) : $length;
+        
+        if (!is_float($length)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        $this->_data['length'] = $length;
+    }
+    
+    /**
+     * Set shot compass bearing.
+     * 
+     * @param float $bearing between 0 and 360 (when unit was grad)
+     * @throws InvalidArgumentException
+     * @todo support other units than grad
+     */
+    public function setBearing($bearing)
+    {
+        // convert to explicit type
+        $bearing = (is_string($bearing)||is_int($bearing))? floatval($bearing) : $bearing;
+        
+        if (!is_float($bearing)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        if ($bearing <0 || $bearing > 360) {
+            // TODO: support other units too
+            throw new InvalidArgumentException(
+                "bearing out of range: $bearing (0->360 grad)"
+            );
+        }
+        
+        $bearing = ($bearing==360)? 0.0 : $bearing; // treat 360 as float(0)
+        $this->_data['bearing'] = $bearing;
+    }
+    
+    /**
+     * Set shot gradient.
+     * 
+     * @param float $gradient between -90 to 90 (when unit was grad)
+     * @throws InvalidArgumentException
+     * @todo support other units than grad
+     */
+    public function setGradient($gradient)
+    {
+        // convert to explicit type
+        $gradient = (is_string($gradient)||is_int($gradient))? floatval($gradient) : $gradient;
+        
+        if (!is_float($gradient)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        if ($gradient <-90 || $gradient > 90) {
+            // TODO: support other units too
+            throw new InvalidArgumentException(
+                "gradient out of range: $gradient (-90->+90 grad)"
+            );
+        }
+        
+        
+        $this->_data['gradient'] = $gradient;
+    }
+    
+    /**
+     * Set shot left dimensions.
+     * 
+     * @param float $left Distance to left wall
+     */
+    public function setLeftDimension($left)
+    {
+        // convert to explicit type
+        $left = (is_string($left)||is_int($left))? floatval($left) : $left;
+        
+        if (!is_float($left)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        $this->_data['left'] = $left;
+    }
+    
+    /**
+     * Set shot right dimensions.
+     * 
+     * @param float $right distance to right wall
+     */
+    public function setRightDimension($right)
+    {
+        // convert to explicit type
+        $right = (is_string($right)||is_int($right))? floatval($right) : $right;
+        
+        if (!is_float($right)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        $this->_data['right'] = $right;
+    }
+    
+    /**
+     * Set shot up (height to ceiling) dimensions.
+     * 
+     * @param float $up distance to ceiling.
+     */
+    public function setUpDimension($up)
+    {
+        // convert to explicit type
+        $up = (is_string($up)||is_int($up))? floatval($up) : $up;
+        
+        if (!is_float($up)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        $this->_data['up'] = $up;
+    }
+    
+    /**
+     * Set shot down (height to ground) dimensions.
+     * 
+     * @param float $down distance to passage ground
+     */
+    public function setDownDimension($down)
+    {
+        // convert to explicit type
+        $down = (is_string($down)||is_int($down))? floatval($down) : $down;
+        
+        if (!is_float($down)) {
+            throw new InvalidArgumentException("Invalid argument type");
+        }
+        $this->_data['down'] = $down;
+    }
+    
+    
+    
+    /**
+     * UTILITY FUNCTIONS
+     */
+     
+    /**
+     * Calculate backwards compass reading.
+     * 
+     * Result=360 will be reported as 0.
+     * 
+     * @return float between 0 and 360 (when unit was grad)
+     * @todo implement other units than grad
+     */
+    public function getBackBearing()
+    {
+        $b = $this->getBearing();
+        if (!is_float($b)) {
+            throw new InvalidArgumentException("bearing '$b' not type float!");
+        }
+        
+        if ($b >= 180.0) {
+            $r = $b-180.0;
+        } else {
+            $r = $b+180.0;
+        }
+
+        return $r;
+    }
+    
+    /**
+     * Calculate backwards clino reading.
+     * 
+     * @return float between -90 and 90 (when unit was grad)
+     */
+    public function getBackGradient()
+    {
+        $b = $this->getGradient();
+        if (!is_float($b)) {
+            throw new InvalidArgumentException("bearing '$b' not type float!");
+        }
+        
+        $r = $b*-1;
+        
+        return $r;
+    }
 }
 
 ?>
---
+++ tests/File_Therion_ShotTest.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Therion shot unit test cases
+ *
+ * PHP version 5
+ *
+ * @category   file
+ * @package    File_Therion_Tests
+ * @author     Benedikt Hallinger <beni@php.net>
+ * @copyright  2016 Benedikt Hallinger
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.txt LGPLv3
+ * @link       http://pear.php.net/package/File_Therion/
+ */
+require_once 'File/Therion.php';  //includepath is loaded by phpUnit from phpunit.xml
+
+/**
+ * PHPUnit test class for testing various datatypes.
+ */
+class File_Therion_ShotTest extends PHPUnit_Framework_TestCase {
+    
+    
+    /**
+     * setup test case, called before a  test is executed.
+     *
+     * @access protected
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Tears down the fixture, for example, close a network connection.
+     * This method is called after a test is executed.
+     *
+     * @access protected
+     */
+    protected function tearDown()
+    {
+    }
+
+
+
+
+/* ---------- TESTS ---------- */
+/* test functions are public and start with "test*".
+
+    /**
+     * dummy test
+     */
+    public function testDummy()
+    {
+        //$this->markTestSkipped('Skipped Test.');
+        //$this->markTestIncomplete("This test has not been implemented yet.");
+    
+        //$this->assertInstanceOf('File_Therion', $testSubject);
+        //$this->assertTrue($false);
+        //$this->assertEquals($expected, $actual, 'Failed!');
+        //$this->assertNotEquals($expected, $actual, 'Failed!');
+        //$this->assertThat(1, $this->greaterThanOrEqual(2));
+
+    }
+    
+
+    /**
+     * Test instantiation
+     */
+    public function testBasicInstantiation()
+    {
+    }
+    
+    /**
+     * Test shot flags
+     */
+    public function testFlags()
+    {
+        $sample = new File_Therion_Shot();
+        $expected = array(
+            'surface'     => false,
+            'splay'       => false,
+            'duplicate'   => false,
+            'approximate' => false,
+            'approx'      => false,  // test alias!
+        );
+        foreach ($expected as $f=>$v) {
+            $this->assertEquals($v, $sample->getFlag($f));
+        }
+        
+        $f = 'surface'; $expected[$f] = true;
+        $sample->setFlag($f, true);
+        foreach ($expected as $f=>$v) {
+            $this->assertEquals($v, $sample->getFlag($f));
+        }
+        
+        $f = 'splay'; $expected[$f] = true;
+        $sample->setFlag($f, true);
+        foreach ($expected as $f=>$v) {
+            $this->assertEquals($v, $sample->getFlag($f));
+        }
+        
+        $f = 'duplicate'; $expected[$f] = true;
+        $sample->setFlag($f, true);
+        foreach ($expected as $f=>$v) {
+            $this->assertEquals($v, $sample->getFlag($f));
+        }
+        
+        $f = 'approx';
+        $expected['approx'] = true; $expected['approximate'] = true;
+        $sample->setFlag($f, true);
+        foreach ($expected as $f=>$v) {
+            $this->assertEquals($v, $sample->getFlag($f));
+        }
+        $f = 'approximate';
+        foreach ($expected as $f=>$v) {
+            $this->assertEquals($v, $sample->getFlag($f));
+        }
+        
+        
+        // TODO: test wrong invocatios
+        
+    }
+    
+    
+    /**
+     * Test parsing
+     */
+    public function testParsing()
+    {
+        // a simple basic survey example
+        $order  = array('from', 'to', 'length', 'bearing', 'gradient');
+        $data   = array('1.1', '1.2', 10, 234, -10);
+        $sample = File_Therion_Shot::parse($data, $order);
+        $this->assertInstanceOf('File_Therion_Shot', $sample);
+        $this->assertEquals(
+            array(
+                '1.1',
+                '1.2',
+                10,
+                234,
+                -10
+            ),
+            array(
+                $sample->getFrom(),
+                $sample->getTo(),
+                $sample->getLength(),
+                $sample->getBearing(),
+                $sample->getGradient(),
+            )
+        );
+    }
+    
+    
+    /**
+     * test backbearing utility function
+     */
+    public function testBackBearing()
+    {
+        foreach (range(0, 360) as $co) {
+            $sample = new File_Therion_Shot();
+            $sample->setBearing($co);
+            $c = $sample->getBearing();  // get possibly converted value
+            $bc = $sample->getBackBearing();
+            
+            $sampleB = new File_Therion_Shot();
+            $sampleB->setBearing($bc);
+            $bco = $sampleB->getBackBearing();
+            
+            $this->assertEquals($c, $bco, "grad=$c (co=$co); bc=$bc; bco=$bco");
+        }
+    }
+    
+    /**
+     * test backgradient utility function
+     */
+    public function testBackGradient()
+    {
+        foreach (range(-90, 90) as $c) {
+            $sample = new File_Therion_Shot();
+            $sample->setGradient($c);
+            $bc = $sample->getBackGradient();
+            
+            $sampleB = new File_Therion_Shot();
+            $sampleB->setGradient($bc);
+            $bco = $sampleB->getBackGradient();
+            
+            $this->assertEquals($c, $bco, "grad=$c; bc=$bc; bco=$bco");
+        }
+    }
+    
+
+}
+?>
